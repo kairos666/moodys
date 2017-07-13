d2a4ccea507fbef4dd5fe500a8793e961ea6a4cd
David Maggi <david.maggi@orange.com>
2017-07-13 21:30:49 +0200

    fix data

--- src/fakeData.js
+++ src/fakeData.js
@@ -1,7 +1,7 @@
 export default {
     users: [
         { id: 0, name: 'Jéremy Barbeau', motto: 'L\'approche GSM first', avatar: 'https://api.adorable.io/avatars/60/jeremy0@adorable.png' },
-        { id: 1, name: 'Thibault Monier', motto: 'J\'aime pas trop les voleurs et les fils de pute', avatar: 'https://api.adorable.io/avatars/60/thibaut1@adorable.png' },
+        { id: 1, name: 'Thibaut Monier', motto: 'J\'aime pas trop les voleurs et les fils de pute', avatar: 'https://api.adorable.io/avatars/60/thibaut1@adorable.png' },
         { id: 2, name: 'Frédéric Vaz e Silva', motto: 'Ils courraient très vite en agitant les bras', avatar: 'https://api.adorable.io/avatars/60/fredo2@adorable.png' },
         { id: 3, name: 'Alexandre Perrault', motto: 'FUPS!!!!!!!', avatar: 'https://api.adorable.io/avatars/60/alex3@adorable.png' },
         { id: 4, name: 'David Maggi', motto: 'Je me suis fait élargir le périmètre un truc de ouf', avatar: 'https://api.adorable.io/avatars/60/david4@adorable.png' },
