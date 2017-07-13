ec69d9f8c6226d3c99aab78ed99de59453072bcd
David Maggi <david.maggi@orange.com>
2017-07-13 23:18:45 +0200

    update dependencies (include firebase) + config

---
+++ src/config/moods.js
@@ -0,0 +1,20 @@
+export default {
+    moodIndexes: ['-5', '-4', '-3', '-2', '-1', '0', '1', '2', '3', '4', '5', 'holiday', 'sick', null],
+    moodLabels: ['apocalyptic', 'horrible', 'very bad', 'bad', 'meh', 'neutral', 'ok', 'good', 'very good', 'crazy good', 'ecstatic', 'away', 'sick', 'no data'],
+    moodImages: [
+        'Emoji_u1f62d.svg',
+        'Emoji_u1f62b.svg',
+        'Emoji_u1f622.svg',
+        'Emoji_u1f615.svg',
+        'Emoji_u1f612.svg',
+        'Emoji_u1f610.svg',
+        'Emoji_u1f60f.svg',
+        'Emoji_u1f642.svg',
+        'Emoji_u1f603.svg',
+        'Emoji_u1f604.svg',
+        'Emoji_u1f923.svg',
+        'Emoji_u1f60e.svg',
+        'Emoji_u1f912.svg',
+        'Emoji_u1f636.svg'
+    ]
+};
--- src/main.js
+++ src/main.js
@@ -3,12 +3,21 @@
 import Vue from 'vue';
 import App from './App';
 import router from './router';
+import firebase from './services/firebase';
+import VueResource from 'vue-resource';
+import Vuefire from 'vuefire';
 
 Vue.config.productionTip = false;
+Vue.use(VueResource);
+Vue.use(Vuefire);
 
 /* eslint-disable no-new */
 new Vue({
     el: '#app',
+    firebase: {
+        users: firebase.database.ref('users'),
+        moods: firebase.database.ref('moods')
+    },
     router,
     template: '<App/>',
     components: { App }
