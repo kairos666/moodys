e960caa6c136a3fc20ab89983a1f72d3399e214d
David Maggi <david.maggi@orange.com>
2017-07-23 16:11:58 +0200

    base profile nav

--- src/components/nano/profile-nav.vue
+++ src/components/nano/profile-nav.vue
@@ -1,21 +1,54 @@
 <template>
     <menu>
-        <button id="demo-menu-lower-right" class="mdl-button mdl-js-button mdl-button--icon">
+        <button class="mdl-button mdl-button--icon" @click.prevent="toggleVisibility">
             <i class="material-icons">more_vert</i>
         </button>
-        <ul class="mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect" for="demo-menu-lower-right">
-            <li class="mdl-menu__item">Some Action</li>
-            <li class="mdl-menu__item">Another Action</li>
-            <li disabled class="mdl-menu__item">Disabled Action</li>
-            <li class="mdl-menu__item">Yet Another Action</li>
-        </ul>
+        <div class="mdl-menu__container" :class="{ 'is-visible': isVisible }">
+            <div class="mdl-menu__outline mdl-menu--bottom-right"></div>
+            <ul class="mdl-menu mdl-menu--bottom-right mdl-menu__outline">
+                <li v-if="!isAuthenticated" class="mdl-menu__item"><router-link to="/authenticate">Sign in</router-link></li>
+                <li v-if="!isAuthenticated" class="mdl-menu__item"><router-link to="/sign-up">Sign up</router-link></li>
+                <li v-if="isAuthenticated" class="mdl-menu__item">
+                    <a href="#" title="Edit my profile">Edit profile</a>
+                </li>
+                <li v-if="isAuthenticated" class="mdl-menu__item--full-bleed-divider"></li>
+                <li v-if="isAuthenticated" class="mdl-menu__item">
+                    <a href="#" title="Sign out my profile">Disconnect</a>
+                </li>
+            </ul>
+        </div>
     </menu>
 </template>
 
 <script>
-    export default {};
+    import { mapGetters } from 'vuex';
+
+    export default {
+        data() {
+            return {
+                isVisible: false
+            };
+        },
+        computed: {
+            ...mapGetters({
+                isAuthenticated: 'isAuthenticated'
+            })
+        },
+        methods: {
+            toggleVisibility() {
+                this.isVisible = !this.isVisible;
+            }
+        }
+    };
 </script>
 
 <style scoped lang="scss">
-    
+    $dd-width: 150px;
+    $dd-height: 120px;
+    .mdl-menu__container { right:18px; top:44px; width:$dd-width; height:$dd-height; }
+    .mdl-menu__outline { width:$dd-width; height:$dd-height; }
+    .is-visible { 
+        .mdl-menu { opacity:1; z-index:999; clip:rect(0 $dd-width $dd-height 0); }
+        .mdl-menu__outline { opacity:1; transform:scale(1); z-index:999; }
+    }
 </style>
