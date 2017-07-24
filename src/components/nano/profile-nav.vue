85a493cd5dd45983869e1f858bda153906e8ff50
David Maggi <david.maggi@orange.com>
2017-07-24 21:29:38 +0200

    styling profile nav links

--- src/components/nano/profile-nav.vue
+++ src/components/nano/profile-nav.vue
@@ -6,14 +6,14 @@
         <div class="mdl-menu__container" :class="{ 'is-visible': isVisible }">
             <div class="mdl-menu__outline mdl-menu--bottom-right"></div>
             <ul class="mdl-menu mdl-menu--bottom-right mdl-menu__outline">
-                <li v-if="!isAuthenticated" class="mdl-menu__item"><router-link to="/authenticate">Sign in</router-link></li>
-                <li v-if="!isAuthenticated" class="mdl-menu__item"><router-link to="/sign-up">Sign up</router-link></li>
+                <li v-if="!isAuthenticated" class="mdl-menu__item"><router-link to="/authenticate"><i class="material-icons">fingerprint</i>Sign in</router-link></li>
+                <li v-if="!isAuthenticated" class="mdl-menu__item"><router-link to="/sign-up"><i class="material-icons">person_add</i>Sign up</router-link></li>
                 <li v-if="isAuthenticated" class="mdl-menu__item">
-                    <a href="#" @click.prevent="onEditProfile" title="Edit my profile">Edit profile</a>
+                    <a href="#" @click.prevent="onEditProfile" title="Edit my profile"><i class="material-icons">account_box</i>Edit profile</a>
                 </li>
                 <li v-if="isAuthenticated" class="mdl-menu__item--full-bleed-divider"></li>
                 <li v-if="isAuthenticated" class="mdl-menu__item">
-                    <a href="#" @click.prevent="onDisconnect" title="Sign out my profile">Disconnect</a>
+                    <a href="#" @click.prevent="onDisconnect" title="Sign out my profile"><i class="material-icons">exit_to_app</i>Disconnect</a>
                 </li>
             </ul>
         </div>
@@ -62,10 +62,17 @@
 </script>
 
 <style scoped lang="scss">
-    $dd-width: 150px;
-    $dd-height: 120px;
+    @import '../../styles/_variables.scss';
+
+    $dd-width: 136px;
+    $dd-height: 96px;
     .mdl-menu__container { right:18px; top:44px; width:$dd-width; height:$dd-height; }
     .mdl-menu__outline { width:$dd-width; height:$dd-height; }
+    .mdl-menu { padding-top:0; padding-bottom:0; }
+    .mdl-menu__item { padding-left:0; padding-right:0;
+        a { display:block; padding-left:$gutter-base*2; padding-right:$gutter-base*2; text-decoration:none; color:$primary; }
+        .material-icons { position:relative; top:7px; margin-right:$gutter-base; }
+    }
     .is-visible { 
         .mdl-menu { opacity:1; z-index:999; clip:rect(0 $dd-width $dd-height 0); }
         .mdl-menu__outline { opacity:1; transform:scale(1); z-index:999; }
