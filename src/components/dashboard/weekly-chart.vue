a53fdc45baf0fa4b7d89e5c94e181cca09292e88
David Maggi <david.maggi@orange.com>
2017-08-08 22:37:49 +0200

    sample of chart formating - need y axis icon labels
    - prepare icon font with emoticons

--- src/components/dashboard/weekly-chart.vue
+++ src/components/dashboard/weekly-chart.vue
@@ -16,24 +16,38 @@
                     datasets: [
                         {
                             label: 'David Maggi',
-                            backgroundColor: '#f87979',
+                            backgroundColor: 'rgba(63, 81, 181, .5)',
                             data: [-4, -3, -2, -1, 0]
                         },
                         {
                             label: 'Jinglu Dai',
-                            backgroundColor: '#f16e00',
+                            backgroundColor: 'rgba(156, 39, 176, .5)',
                             data: [5, -5, 4, -4, 0]
                         },
                         {
                             label: 'Laurent Maggi',
-                            backgroundColor: '#797979',
+                            backgroundColor: 'rgba(0, 150, 136, .5)',
                             data: [5, 4, -2, 1, -5]
                         }
                     ]
                 },
                 barChartOptions: {
                     responsive: true,
-                    maintainAspectRatio: false
+                    maintainAspectRatio: false,
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                fontFamily: 'Material Icons',
+                                callback: function(value, index, values) {
+                                    // match each value with corresponding font icon
+                                    // console.log('value: ', value);
+                                    // console.log('values: ', values);
+                                    // console.log('index: ', index);
+                                    return '';
+                                }
+                            }
+                        }]
+                    }
                 }
             };
         },
