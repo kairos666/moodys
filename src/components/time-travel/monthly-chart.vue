c4e26a5be1a04c20a60dac2b28ad8c9a18b96770
David M <musashi.miyamoto81@gmail.com>
2017-09-23 17:15:05 +0200

    time travel monthly chart

--- src/components/time-travel/monthly-chart.vue
+++ src/components/time-travel/monthly-chart.vue
@@ -11,10 +11,9 @@
     import LineChart from '@/components/nano/reactive-line-chart';
     
     export default {
-        props: ['datasets', 'full-week'],
+        props: ['datasets'],
         data() {
             return {
-                labels: (this.fullWeek) ? ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'saturday', 'sunday'] : ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
                 lineChartOptions: {
                     responsive: true,
                     maintainAspectRatio: false,
@@ -50,6 +49,11 @@
             };
         },
         computed: {
+            labels() {
+                let results = ['1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th', '10th', '11th', '12th', '13th', '14th', '15th', '16th', '17th', '18th', '19th', '20th', '21st', '22nd', '23rd', '24th', '25th', '26th', '27th', '28th', '29th', '30th', '31st'];
+                results.splice(this.datasets[0].data.length);
+                return results;
+            },
             computedChartData() {
                 if (!this.datasets) return {};
 
