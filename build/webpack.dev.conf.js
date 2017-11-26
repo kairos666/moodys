9987493169a94e186023031439756ed69934cba3
Unknown <musashi.miyamoto81@gmail.com>
2017-11-26 21:52:07 +0100

    [dev] enable specific dev service worker
    will enable to test push notifications

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -7,6 +7,7 @@ var merge = require('webpack-merge')
 var baseWebpackConfig = require('./webpack.base.conf')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 var FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
+var SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin')
 
 // add hot-reload related code to entry chunks
 Object.keys(baseWebpackConfig.entry).forEach(function (name) {
@@ -34,6 +35,14 @@ module.exports = merge(baseWebpackConfig, {
       serviceWorkerLoader: `<script>${fs.readFileSync(path.join(__dirname,
         './service-worker-dev.js'), 'utf-8')}</script>`
     }),
-    new FriendlyErrorsPlugin()
+    new FriendlyErrorsPlugin(),
+    // service worker caching
+    new SWPrecacheWebpackPlugin({
+      cacheId: 'moodies-dev-app',
+      filename: 'service-worker.js',
+      staticFileGlobs: ['cache-none'],
+      minify: true,
+      stripPrefix: 'dist/'
+    })
   ]
 })
