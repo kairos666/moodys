9e55ad24dff32ea099d087e09824b28ab734528f
Unknown <musashi.miyamoto81@gmail.com>
2017-12-09 08:14:05 +0100

    [SW] update for push

--- build/service-worker-dev.js
+++ build/service-worker-dev.js
@@ -4,7 +4,24 @@
 // file that will precache your site's local assets.
 // See https://github.com/facebookincubator/create-react-app/issues/2272#issuecomment-302832432
 
+const isLocalhost = Boolean(window.location.hostname === 'localhost' ||
+// [::1] is the IPv6 localhost address.
+window.location.hostname === '[::1]' ||
+// 127.0.0.1/8 is considered localhost for IPv4.
+window.location.hostname.match(
+  /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+)
+);
+
 self.addEventListener('install', () => self.skipWaiting());
+
+window.addEventListener('load', function() {
+  if ('serviceWorker' in navigator && (window.location.protocol === 'https:' || isLocalhost)) {
+    navigator.serviceWorker.register('service-worker.js').then(function(registration) {
+      console.log('Service Worker');
+    });
+  }
+});
 
 self.addEventListener('activate', () => {
   self.clients.matchAll({ type: 'window' }).then(windowClients => {
@@ -13,5 +30,16 @@ self.addEventListener('activate', () => {
       // fresh navigation response from the local dev server.
       windowClient.navigate(windowClient.url);
     }
+  });
+
+  self.addEventListener('push', function(event) {
+    console.log('Received push notif', event);
+    var notificationTitle = 'Hello';
+    var notificationOptions = {
+      body: 'Thanks for sending this push msg.',
+      tag: 'simple-push-demo-notification'
+    };
+  
+    event.waitUntil(self.registration.showNotification(notificationTitle, notificationOptions));
   });
 });
