47537a0ef0ebd8926ce844a37edacbd02c5228b2
Unknown <musashi.miyamoto81@gmail.com>
2017-12-31 11:14:03 +0100

    [WP server] config to use wedeploy server

--- src/config/web-push.js
+++ src/config/web-push.js
@@ -1,7 +1,7 @@
 let webPush = {
     apiServerKey: 'opensesame',
     apiServerKeyHeaderName: 'x-api-key',
-    serverURL: 'http://localhost:3030/push-notification/',
+    serverURL: 'https://notifications-notifications.wedeploy.io/push-notification/',
     vapidPublicKey: 'BFx9xYoe2fg5q3j0GUTgbL59MdxMmOIdX0KTRYpntTnIKaZCH0YIObpCo71sX8PiEkliXeYQtQeHZl_PxmC-bYA',
     notifClickedURL: 'https://moodies-1ad4f.firebaseapp.com/', // should match app host
     badgeIcon: 'static/img/icons/notification-icon.png'
