002178f42bb3b478c7006deb8573478555b129da
Unknown <musashi.miyamoto81@gmail.com>
2018-01-28 17:55:10 +0100

    [badges] add badges and map to achievment cards

--- src/components/nano/badge-card.vue
+++ src/components/nano/badge-card.vue
@@ -2,9 +2,11 @@
     <flip-card>
         <div class="badge-card-side badge-card-side--front" slot="front">
             <h3>{{info.title}}</h3>
+            <img :alt="`badge icon - ${info.badge.replace('-', ' ')}`" :src="`static/img/badges/${info.badge}.svg`" />
         </div>
         <div class="badge-card-side badge-card-side--back" slot="back">
             <p>{{info.description}}</p>
+            <img :alt="`badge icon - ${info.badge.replace('-', ' ')}`" :src="`static/img/badges/${info.badge}.svg`" />
         </div>
     </flip-card>
 </template>
@@ -27,8 +29,9 @@
     @import '../../styles/_include-media.scss';
     @import '../../styles/_variables.scss';
 
-    .badge-card-side { color:#fff; padding:35px; 
-        h3 { font-size:1.8em; margin:0 0 $gutter-base; text-align:center; }
+    .badge-card-side { color:#fff; padding:35px; text-align:center;
+        h3 { font-size:1.8em; margin:0 0 $gutter-base; }
         p { font-size:1.2em; text-align:center; }
+        img { width:100px; opacity:0.25; filter: grayscale(1); }
     }
 </style>
