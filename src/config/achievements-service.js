2f84880862c454ed0a9aa1cb3f534d0e6ff352f2
Unknown <musashi.miyamoto81@gmail.com>
2018-04-01 15:37:45 +0200

    [badges] link to prod achievements service

--- src/config/achievements-service.js
+++ src/config/achievements-service.js
@@ -1,7 +1,7 @@
 let achievementsService = {
-    apiServerKey: 'opensesame',
+    apiServerKey: 'XUocZ8HVQAsqz8273aEEmW75QPs7gjHIV9oKW36b',
     apiServerKeyHeaderName: 'x-api-key',
-    serverURL: 'http://127.0.0.1:3030/special-events-achievements/'
+    serverURL: 'https://achievements-moodysbackend.wedeploy.io/special-events-achievements/'
 };
 
 export default achievementsService;
