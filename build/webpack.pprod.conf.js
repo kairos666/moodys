779ce9349ead107972ca967bc3dbc9f3d4f3afbb
Unknown <musashi.miyamoto81@gmail.com>
2018-04-15 02:08:18 +0200

    [notifs achievements actions] fix

--- build/webpack.pprod.conf.js
+++ build/webpack.pprod.conf.js
@@ -14,168 +14,164 @@ var SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin')
 var env = config.pprodbuild.env
 
 var webpackConfig = merge(baseWebpackConfig, {
-  module: {
-    rules: utils.styleLoaders({
-      sourceMap: config.pprodbuild.productionSourceMap,
-      extract: true
-    }).concat([
-      {
-        loader: 'string-replace-loader',
-        test: /\.(js|vue|json)$/,
-        exclude: /node_modules/,
-        options: {
-          multiple: config.pprodbuild.replaceOccurrences.map(replaceItem => {
-            return { search: replaceItem.from, replace: replaceItem.to, flags: 'g' }
-          })
-        }
-      }
-    ])
-  },
-  devtool: config.pprodbuild.productionSourceMap ? '#source-map' : false,
-  output: {
-    path: config.pprodbuild.assetsRoot,
-    filename: utils.assetsPath('js/[name].[chunkhash].js'),
-    chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
-  },
-  plugins: [
-    // http://vuejs.github.io/vue-loader/en/workflow/production.html
-    new webpack.DefinePlugin({
-      'process.env': env
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
-      },
-      sourceMap: true
-    }),
-    // extract css into its own file
-    new ExtractTextPlugin({
-      filename: utils.assetsPath('css/[name].[contenthash].css')
-    }),
-    // Compress extracted CSS. We are using this plugin so that possible
-    // duplicated CSS from different components can be deduped.
-    new OptimizeCSSPlugin({
-      cssProcessorOptions: {
-        safe: true
-      }
-    }),
-    // generate dist index.html with correct asset hash for caching.
-    // you can customize output by editing /index.html
-    // see https://github.com/ampedandwired/html-webpack-plugin
-    new HtmlWebpackPlugin({
-      filename: config.pprodbuild.index,
-      template: 'index.html',
-      inject: true,
-      minify: {
-        removeComments: true,
-        collapseWhitespace: true,
-        removeAttributeQuotes: true
-        // more options:
-        // https://github.com/kangax/html-minifier#options-quick-reference
-      },
-      // necessary to consistently work with multiple chunks via CommonsChunkPlugin
-      chunksSortMode: 'dependency',
-      serviceWorkerLoader: `<script>${fs.readFileSync(path.join(__dirname,
+    module: {
+        rules: utils.styleLoaders({
+            sourceMap: config.pprodbuild.productionSourceMap,
+            extract: true
+        }).concat([{
+            loader: 'string-replace-loader',
+            test: /\.(js|vue|json)$/,
+            exclude: /node_modules/,
+            options: {
+                multiple: config.pprodbuild.replaceOccurrences.map(replaceItem => {
+                    return { search: replaceItem.from, replace: replaceItem.to, flags: 'g' }
+                })
+            }
+        }])
+    },
+    devtool: config.pprodbuild.productionSourceMap ? '#source-map' : false,
+    output: {
+        path: config.pprodbuild.assetsRoot,
+        filename: utils.assetsPath('js/[name].[chunkhash].js'),
+        chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
+    },
+    plugins: [
+        // http://vuejs.github.io/vue-loader/en/workflow/production.html
+        new webpack.DefinePlugin({
+            'process.env': env
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            },
+            sourceMap: true
+        }),
+        // extract css into its own file
+        new ExtractTextPlugin({
+            filename: utils.assetsPath('css/[name].[contenthash].css')
+        }),
+        // Compress extracted CSS. We are using this plugin so that possible
+        // duplicated CSS from different components can be deduped.
+        new OptimizeCSSPlugin({
+            cssProcessorOptions: {
+                safe: true
+            }
+        }),
+        // generate dist index.html with correct asset hash for caching.
+        // you can customize output by editing /index.html
+        // see https://github.com/ampedandwired/html-webpack-plugin
+        new HtmlWebpackPlugin({
+            filename: config.pprodbuild.index,
+            template: 'index.html',
+            inject: true,
+            minify: {
+                removeComments: true,
+                collapseWhitespace: true,
+                removeAttributeQuotes: true
+                    // more options:
+                    // https://github.com/kangax/html-minifier#options-quick-reference
+            },
+            // necessary to consistently work with multiple chunks via CommonsChunkPlugin
+            chunksSortMode: 'dependency',
+            serviceWorkerLoader: `<script>${fs.readFileSync(path.join(__dirname,
         './service-worker-pprod.js'), 'utf-8')}</script>`
-    }),
-    // split vendor js into its own file
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      minChunks: function (module, count) {
-        // any required modules inside node_modules are extracted to vendor
-        return (
-          module.resource &&
-          /\.js$/.test(module.resource) &&
-          module.resource.indexOf(
-            path.join(__dirname, '../node_modules')
-          ) === 0
-        )
-      }
-    }),
-    // extract webpack runtime and module manifest to its own file in order to
-    // prevent vendor hash from being updated whenever app bundle is updated
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'manifest',
-      chunks: ['vendor']
-    }),
-    // copy custom static assets
-    new CopyWebpackPlugin([
-      {
-        from: path.resolve(__dirname, '../static'),
-        to: config.pprodbuild.assetsSubDirectory,
-        ignore: ['.*'],
-        transform(content, path) {
-          // early exit if file to be copied is not js or json
-          if (!path.match(/\.(js|json)$/)) return content;
+        }),
+        // split vendor js into its own file
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor',
+            minChunks: function(module, count) {
+                // any required modules inside node_modules are extracted to vendor
+                return (
+                    module.resource &&
+                    /\.js$/.test(module.resource) &&
+                    module.resource.indexOf(
+                        path.join(__dirname, '../node_modules')
+                    ) === 0
+                )
+            }
+        }),
+        // extract webpack runtime and module manifest to its own file in order to
+        // prevent vendor hash from being updated whenever app bundle is updated
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'manifest',
+            chunks: ['vendor']
+        }),
+        // copy custom static assets
+        new CopyWebpackPlugin([{
+            from: path.resolve(__dirname, '../static'),
+            to: config.pprodbuild.assetsSubDirectory,
+            ignore: ['.*'],
+            transform(content, path) {
+                // early exit if file to be copied is not js or json
+                if (!path.match(/\.(js|json)$/)) return content;
 
-          let stringContent = content.toString();
+                let stringContent = content.toString();
 
-          config.pprodbuild.replaceOccurrences.forEach(replaceItem => {
-            stringContent = stringContent.replace(new RegExp(replaceItem.from, 'g'), replaceItem.to);
-          });
+                config.pprodbuild.replaceOccurrences.forEach(replaceItem => {
+                    stringContent = stringContent.replace(new RegExp(replaceItem.from, 'g'), replaceItem.to);
+                });
 
-          return stringContent;
-        }
-      }
-    ]),
-    // service worker caching
-    new SWPrecacheWebpackPlugin({
-      cacheId: 'moodies-app',
-      filename: 'service-worker.js',
-      staticFileGlobs: ['dist/**/*.{js,html,css,svg,eot,ttf,woff,png,jpg,jpeg}'],
-      minify: true,
-      importScripts: [
-        /* './static/sw/config.js', */
-        './static/sw/push-handler.js',
-        './static/sw/notif-click-handler.js'
-      ],
-      stripPrefix: 'dist/',
-      runtimeCaching: [
-      {
-        urlPattern: /^https:\/\/fonts\.googleapis\.com\//,
-        handler: 'cacheFirst'
-      },
-      {
-        urlPattern: /^https:\/\/fonts\.gstatic\.com\//,
-        handler: 'cacheFirst'
-      },
-      {
-        urlPattern: /^https:\/\/code\.getmdl\.io\//,
-        handler: 'cacheFirst'
-      },
-      {
-        urlPattern: /^https:\/\/api\.adorable\.io\//,
-        handler: 'cacheFirst'
-      },
-      {
-        urlPattern: /^https:\/\/www\.gravatar\.com\//,
-        handler: 'networkFirst'
-      }]
-    })
-  ]
+                return stringContent;
+            }
+        }]),
+        // service worker caching
+        new SWPrecacheWebpackPlugin({
+            cacheId: 'moodies-app',
+            filename: 'service-worker.js',
+            staticFileGlobs: ['dist/**/*.{js,html,css,svg,eot,ttf,woff,png,jpg,jpeg}'],
+            minify: false,
+            importScripts: [
+                './static/sw/config.js',
+                './static/sw/push-handler.js',
+                './static/sw/notif-click-handler.js'
+            ],
+            stripPrefix: 'dist/',
+            runtimeCaching: [{
+                    urlPattern: /^https:\/\/fonts\.googleapis\.com\//,
+                    handler: 'cacheFirst'
+                },
+                {
+                    urlPattern: /^https:\/\/fonts\.gstatic\.com\//,
+                    handler: 'cacheFirst'
+                },
+                {
+                    urlPattern: /^https:\/\/code\.getmdl\.io\//,
+                    handler: 'cacheFirst'
+                },
+                {
+                    urlPattern: /^https:\/\/api\.adorable\.io\//,
+                    handler: 'cacheFirst'
+                },
+                {
+                    urlPattern: /^https:\/\/www\.gravatar\.com\//,
+                    handler: 'networkFirst'
+                }
+            ]
+        })
+    ]
 })
 
 if (config.pprodbuild.productionGzip) {
-  var CompressionWebpackPlugin = require('compression-webpack-plugin')
+    var CompressionWebpackPlugin = require('compression-webpack-plugin')
 
-  webpackConfig.plugins.push(
-    new CompressionWebpackPlugin({
-      asset: '[path].gz[query]',
-      algorithm: 'gzip',
-      test: new RegExp(
-        '\\.(' +
-        config.pprodbuild.productionGzipExtensions.join('|') +
-        ')$'
-      ),
-      threshold: 10240,
-      minRatio: 0.8
-    })
-  )
+    webpackConfig.plugins.push(
+        new CompressionWebpackPlugin({
+            asset: '[path].gz[query]',
+            algorithm: 'gzip',
+            test: new RegExp(
+                '\\.(' +
+                config.pprodbuild.productionGzipExtensions.join('|') +
+                ')$'
+            ),
+            threshold: 10240,
+            minRatio: 0.8
+        })
+    )
 }
 
 if (config.pprodbuild.bundleAnalyzerReport) {
-  var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
-  webpackConfig.plugins.push(new BundleAnalyzerPlugin())
+    var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+    webpackConfig.plugins.push(new BundleAnalyzerPlugin())
 }
 
 module.exports = webpackConfig
--- static/sw/notif-click-handler.js
+++ static/sw/notif-click-handler.js
@@ -5,37 +5,43 @@ self.addEventListener('notificationclick', function(event) {
     let clickResponsePromise = Promise.resolve();
     if (event.notification.data && event.notification.data.url) {
         clickResponsePromise = clients.matchAll().then(function(clis) {
-        if (event.notification.data && event.notification.data.url) {
-            let visibleClient = clis.find(function(c) {
-            return c.visibilityState === 'visible';
-            });
-            if (visibleClient !== undefined) {
-            // display the page in the visible client
-            visibleClient.navigate(event.notification.data.url);
-            return visibleClient.focus();
-            } else {
-            // there are no visible windows. Open one.
-            return clients.openWindow(event.notification.data.url);
+            if (event.notification.data && event.notification.data.url) {
+                let visibleClient = clis.find(function(c) {
+                    return c.visibilityState === 'visible';
+                });
+                if (visibleClient !== undefined) {
+                    // display the page in the visible client
+                    visibleClient.navigate(event.notification.data.url);
+                    return visibleClient.focus();
+                } else {
+                    // there are no visible windows. Open one.
+                    return clients.openWindow(event.notification.data.url);
+                }
             }
-        }
-        event.notification.close();
+            event.notification.close();
         });
     };
 
     // ajax call to backend special events
-    // let ajaxConfig = self._config.achievementsbackend;
-    // let pajax = fetch(`${ajaxConfig.baseURL}/${ajaxConfig.endPoints.specialEvts}`, {
-    //     mode: 'cors',
-    //     method: 'post',
-    //     headers: ajaxConfig.baseHeaders,
-    //     body: ajaxConfig.bodyBuilder('notification-click', 'notif-action', event.notification.data.uid, event.notification.data.toUid, event.notification.data.pushTimestamp)
-    // });
+    let ajaxConfig = _config.achievementsbackend;
+    let pajax = fetch(`${ajaxConfig.baseURL}/${ajaxConfig.endPoints.specialEvts}`, {
+        mode: 'cors',
+        method: 'post',
+        headers: ajaxConfig.baseHeaders,
+        body: ajaxConfig.bodyBuilder('notification-click', 'notif-action', event.notification.data.uid, event.notification.data.toUid, event.notification.data.pushTimestamp)
+    }).then(() => {
+        console.log('successful backend service call from SW');
+        return Promise.resolve();
+    }).catch(() => {
+        console.log('failed backend service call from SW');
+        // make it a success anyway - we only need the service worker to be active until it is resolved
+        return Promise.resolve();
+    });
 
-    // event.waitUntil(
-    //     Promise.all([
-    //         clickResponsePromise,
-    //         pajax
-    //     ])
-    // );
-    event.waitUntil(clickResponsePromise); 
+    event.waitUntil(
+        Promise.all([
+            clickResponsePromise,
+            pajax
+        ])
+    );
 });
