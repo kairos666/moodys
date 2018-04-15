28de4956384a1c9d994f7a32413a768e31fc6e26
Unknown <musashi.miyamoto81@gmail.com>
2018-04-15 21:25:37 +0200

    [component tester] for test purposes should be cleaned from code thereafter

---
+++ src/components/nano/component-tester.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <water-tank-mood-indicator
+            :today-mood="mockDayMood" 
+            :week-mood="mockWeekMood"
+        ></water-tank-mood-indicator>
+    </div>
+</template>
+
+<script>
+    import WaterTankMoodIndicator from '@/components/nano/water-tank-mood-indicator';
+
+    export default {
+        data() {
+            return {
+                mockDayMood: '0',
+                mockWeekMood: '0'
+            };
+        },
+        components: {
+            WaterTankMoodIndicator
+        }
+    };
+</script>
