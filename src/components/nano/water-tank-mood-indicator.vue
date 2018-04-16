d927e3038c5bc152e354e0a1c937d64232e1e838
Unknown <musashi.miyamoto81@gmail.com>
2018-04-16 23:10:20 +0200

    [water tank] generate mood indicator

--- src/components/nano/water-tank-mood-indicator.vue
+++ src/components/nano/water-tank-mood-indicator.vue
@@ -8,11 +8,15 @@
                 </v-group>
             </v-layer>
             <v-layer ref="bubblesLayer"></v-layer>
+            <v-layer ref="moodLayer">
+                <v-group ref="moodEmojiIndicator"></v-group>
+            </v-layer>
         </v-stage>
     </figure>
 </template>
 
 <script>
+    import EmojiHelpers from '@/utils/emoji-helpers';
     // setup vue-konva plugin
     import Vue from 'vue';
     import VueKonva from 'vue-konva';
@@ -25,7 +29,8 @@
             return {
                 model: {
                     waterLevel: 0,
-                    moodLevel: 0.5
+                    moodLevel: 0.5,
+                    moodEmojiIndex: EmojiHelpers.emojiDataArray.findIndex(item => (item.index === null))
                 },
                 stageWidth: 0,
                 stageHeight: 0,
@@ -39,12 +44,19 @@
                 bubblesPeriod: 2000,
                 bubblesAmplitude: 1 / 6,
                 bubblesCollection: [],
+                moodEmojisCollection: [],
                 tweenersCollection: [],
                 animationsCollection: [],
                 waterLineBaseOptions: { points: [], fill: '#ffffff', bezier: true, tension: 0.35, closed: true, name: 'waterline' },
                 bubbleBaseOptions: {
                     bubbleOuter: { x: -50, y: -50, fill: '#ffffff', visible: 'inherit', data: 'M50,0C22.386,0,0,22.386,0,50s22.386,50,50,50s50-22.386,50-50S77.614,0,50,0z M50,97.5 C23.767,97.5,2.5,76.233,2.5,50S23.767,2.5,50,2.5S97.5,23.767,97.5,50S76.233,97.5,50,97.5z' },
                     bubbleInner: { x: -50, y: -50, fill: '#ffffff', visible: 'inherit', data: 'M72.846,10.525C62.055,4.126,51.13,3.281,47.973,8.603c-2.775,4.68-0.039,6.486,13.441,11.258 c2.085,0.738,6.515,3.302,7.67,4.403c9.226,8.799,14.219,12.486,17.375,7.165S83.637,16.925,72.846,10.525z M83.979,29.958 c-2.36,3.979-7.678-5.271-16.764-10.66c-9.087-5.389-19.121-5.245-16.762-9.224c2.36-3.979,12.029-2.788,21.116,2.602 C80.656,18.065,86.339,25.979,83.979,29.958z' }
+                },
+                moodEmojiBaseOptions: {
+                    width: 100,
+                    height: 100,
+                    x: -50,
+                    y: -45
                 }
             };
         },
@@ -88,6 +100,17 @@
                     }
                 },
                 immediate: true
+            },
+            todayMood: {
+                handler: function(val) {
+                    // keep final state change
+                    this.model.moodLevel = this.propToPercentConverter(val);
+                    this.model.moodEmojiIndex = this.propToEmojiIndexConverter(val);
+
+                    // animate mood level change (when mounted)
+                    console.log('TODO mood animation');
+                },
+                immediate: true
             }
         },
         methods: {
@@ -125,6 +148,36 @@
                 bubble.add(bubbleOuter, bubbleInner);
 
                 return bubble;
+            },
+            moodEmojiIndicatorBuilder(groupHolder) {
+                // generate emoji image collection (only matching index is visible)
+                const pImagesArray = EmojiHelpers.emojiDataArray
+                    .map(emojiData => `/static/img/smileys/${emojiData.image}`)
+                    .map((emojiURL, index) => {
+                        return new Promise(resolve => {
+                            const moodImage = new Image();
+                            moodImage.onload = () => resolve({ path: emojiURL, status: 'ok', image: moodImage });
+                            moodImage.onerror = () => resolve({ path: emojiURL, status: 'fail', image: moodImage });
+                            moodImage.src = emojiURL;
+                        });
+                    });
+
+                // handle when all images have been loaded
+                Promise.all(pImagesArray).then(responses => {
+                    // assign images to collection
+                    this.moodEmojisCollection = responses.map((resp, index) => {
+                        return new Konva.Image(Object.assign({}, this.moodEmojiBaseOptions, {
+                            visible: (this.model.moodEmojiIndex === index),
+                            image: resp.image
+                        }));
+                    });
+                    // add images to group then add to layer ... then redraw to make them appear
+                    groupHolder.add(...this.moodEmojisCollection);
+                    this.$refs.stage.getStage().draw();
+                });
+            },
+            propToEmojiIndexConverter(moodScore) {
+                return EmojiHelpers.emojiDataArray.findIndex(item => (item.index === moodScore));
             },
             propToPercentConverter(moodScore) {
                 if (moodScore === null || moodScore === 'sick' || moodScore === 'holiday') {
@@ -196,6 +249,11 @@
                 });
                 // animations
                 this.bubblesAnimation(this.bubblesCollection, this.$refs.bubblesLayer.getStage());
+
+                /** mood indicator **/
+                // reset properties
+                const moodEmojiIndicator = this.$refs.moodEmojiIndicator.getStage();
+                moodEmojiIndicator.setAttrs({ x: this.stageWidth / 2, y: this.percentToStageHeightConverter(this.model.moodLevel) });
             },
             destroyAnimations() {
                 // when component is destroyed or resized
@@ -221,12 +279,16 @@
             // fine tuning for performances
             this.$refs.waterLayer.getStage().hitGraphEnabled(false);
             const bubblesLayer = this.$refs.bubblesLayer.getStage().hitGraphEnabled(false);
+            this.$refs.moodLayer.getStage().hitGraphEnabled(false);
 
             // add generated bubbles to stage
             for (let i = 0; i < this.bubblesCount; i++) {
                 this.bubblesCollection.push(this.bubbleBuilder());
             }
             bubblesLayer.add(...this.bubblesCollection);
+
+            // add generated mood emoji indicator to stage
+            this.moodEmojiIndicatorBuilder(this.$refs.moodEmojiIndicator.getStage());
 
             // setup resize behavior
             window.addEventListener('resize', this.resizeStage);
