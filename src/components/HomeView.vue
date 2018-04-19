371d467c5ee1485df2094fde0cf5dea4c56c217c
David Maggi <david.maggi@orange.com>
2018-04-19 23:16:25 +0200

    [TEST BED] adapted test bed to new refactore component

--- src/components/HomeView.vue
+++ src/components/HomeView.vue
@@ -80,7 +80,14 @@
                                     :today-mood="$store.getters.currentUserMood" 
                                     :week-mood="$store.getters.currentUserWeekMoods.weekAverage"
                                 ></water-tank-mood-indicator> -->
-                                <component-tester></component-tester>
+                                <!-- <component-tester></component-tester> -->
+                            </div>
+                        </home-card>
+                    </li>
+                    <li>
+                        <home-card class="home-card__mood-box home-card__mood-box-allusers">
+                            <div slot="description">
+                                <component-tester-v2></component-tester-v2>
                             </div>
                         </home-card>
                     </li>
@@ -119,8 +126,8 @@
     import DoubleMoodBox from '@/components/dashboard/double-mood-box';
     import CompletionRate from '@/components/dashboard/completion-rate';
     import WeeklyChart from '@/components/dashboard/weekly-chart';
-    import WaterTankMoodIndicator from '@/components/nano/water-tank-mood-indicator';
     import ComponentTester from '@/components/nano/component-tester';
+    import ComponentTesterV2 from '@/components/nano/component-tester-v2';
     import timeHelpers from '@/utils/time-helpers';
 
     export default {
@@ -159,8 +166,8 @@
             'double-mood-box': DoubleMoodBox,
             'completion-rate': CompletionRate,
             'weekly-chart': WeeklyChart,
-            'water-tank-mood-indicator': WaterTankMoodIndicator,
-            'component-tester': ComponentTester
+            'component-tester': ComponentTester,
+            'component-tester-v2': ComponentTesterV2
         }
     };
 </script>
