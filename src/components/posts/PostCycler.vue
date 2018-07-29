ca361370dec0ef11d411de2b28fc5e2a22cdb71c
David Maggi <david.maggi@orange.com>
2018-07-29 18:13:00 +0200

    [posts] post cycling on dashboard

---
+++ src/components/posts/PostCycler.vue
@@ -0,0 +1,34 @@
+<template>
+    <transition name="fade">
+        <post v-if="posts.length !== 0" single :post-data="posts[index]"></post>
+        <p v-else>There are no twoots yet.</p>
+    </transition>
+</template>
+
+<script>
+    import Post from '@/components/posts/Post';
+
+    export default {
+        props: {
+            index: {
+                type: Number,
+                default: 0
+            },
+            posts: {
+                type: Array,
+                required: true
+            }
+        },
+        components: {
+            Post
+        }
+    };
+</script>
+
+<style scoped lang="scss">
+    @import '../../styles/_variables.scss';
+    @import '../../styles/_utils.scss';
+
+    .fade-enter-active, .fade-leave-active { transition: all .5s ease; }
+    .fade-enter, .fade-leave-to { opacity: 0; transform: translateX(100%); }
+</style>
